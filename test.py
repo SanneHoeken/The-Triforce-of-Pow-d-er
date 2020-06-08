b9cb9b68835303ce609c9de11795baf56a6a10e0
add-more-ducks <thomas.van.genderen@student.uva.nl>
2020-06-08 13:04:53 +0200

    Added x_range, y_range to test.py

--- test.py
+++ test.py
@@ -45,11 +45,16 @@ if __name__ == "__main__":
     x_C = [amino.coordinate[0] for amino in protein.aminos if amino.type == 'C']
     y_C = [amino.coordinate[1] for amino in protein.aminos if amino.type == 'C']
 
+    x_range = round(0.5 * len(protein.aminos))
+    y_range = round(0.5 * len(protein.aminos))
+
+    print(x_range)
+
     plt.figure()
-    plt.xlim(-10, 10)
-    plt.ylim(-10, 10)
-    plt.xticks(range(-10, 10))
-    plt.yticks(range(-10, 10))
+    plt.xlim(-x_range, x_range)
+    plt.ylim(-y_range, y_range)
+    plt.xticks(range(-x_range, x_range))
+    plt.yticks(range(-y_range, y_range))
     plt.grid(b=True)
     plt.plot(x, y, color='k')
     plt.scatter(x_H, y_H, color='r', marker='o')
