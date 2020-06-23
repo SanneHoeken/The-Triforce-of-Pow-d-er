834af5dd5a7529e798b37481d51bfa29d23f7fdd
Miloceane <lafage.cha@gmail.com>
2020-06-23 17:10:36 +0200

    Greedy, Random, Hill Climber and Simulated Annealing are code reviewed

--- code/algorithms/greedy.py
+++ code/algorithms/greedy.py
@@ -18,24 +18,24 @@ class BestGreedy():
         Folds protein greedily for every iteration, stores its values if configuration has a lower score,
         and then resets protein. After the iterations, the best values are set in the protein.
         """
-        # fold protein iterations times 
+        # Fold protein iterations times 
         for i in range(self.iterations):
             greedy_folder = Greedy(self.protein)
             greedy_folder.fold_protein()
 
-            # calculate score
+            # Calculate score
             score = greedy_folder.protein.get_score()
 
-            # update protein values en score if score is lower
+            # Update protein values en score if score is lower
             if score < self.best_score:
                 self.best_score = score
                 self.best_protein = [(amino.fold, amino.coordinate, amino.previous_amino) for amino in greedy_folder.protein.get_aminos()]
 
-            # reset protein
+            # Reset protein
             for amino in self.protein.get_aminos()[1:]:
                 amino.reset_amino()
 
-        # set values of protein to best values
+        # Set values of protein to best values
         for i, values in enumerate(self.best_protein):
             self.protein.get_aminos()[i].set_fold(values[0])
             self.protein.get_aminos()[i].set_coordinate(values[1])
@@ -53,36 +53,36 @@ class Greedy(RandomFolder):
         """
         Retrieves values for amino by choosing the best values out of possible values
         """
-        # retrieve all possible values for amino
+        # Retrieve all possible values for amino
         possible_values = self.get_possible_values(amino)  
         
-        # return None if no possible values
+        # Return None if no possible values
         if len(possible_values) == 0:
             return None
 
         best_values = []
         best_score = 1
 
-        # get best of possible values
+        # Get best of possible values
         for values in possible_values:
     
-            # fold amino according to fold values
-            # store score of new folded configuration
+            # Fold amino according to fold values
+            # Store score of new folded configuration
             score = self.set_values(values, self.folding)
 
-            # replace best values if score is lower
+            # Replace best values if score is lower
             if score < best_score:
                 best_score = score
                 best_values = [values]
             
-            # add best values if score is as low as lowest
+            # Add best values if score is as low as lowest
             elif score == best_score:
                 best_values.append(values)
 
-            # undo amino folding
+            # Undo amino folding
             self.reset_values(self.folding)       
         
-        # return one of best values (randomly chosen)
+        # Return one of best values (randomly chosen)
         return random.choice(best_values)
 
 
@@ -91,8 +91,8 @@ class Greedy(RandomFolder):
         Resets fold of amino with specified id, resets coordinate and 
         previous fold of that amino's next neighbor 
         """
-        # reset fold of current amino
+        # Reset fold of current amino
         self.protein.get_aminos()[id].set_fold(None)
 
-        # reset coordinate and previous amino of next amino
+        # Reset coordinate and previous amino of next amino
         self.protein.get_aminos()[id + 1].reset_amino()
--- code/algorithms/random.py
+++ code/algorithms/random.py
@@ -17,24 +17,24 @@ class BestOfRandom():
         Folds protein randomly for every iteration, stores its values if configuration has a lower score,
         and then resets protein. After the iterations, the best values are set in the protein.
         """
-        # fold protein iterations times 
+        # Fold protein iterations times
         for i in range(self.iterations):
             random_folder = RandomFolder(self.protein)
             random_folder.fold_protein()
 
-            # calculate score
+            # Calculate score
             score = random_folder.protein.get_score()
 
-            # update protein values en score if score is lower
+            # Update protein values en score if score is lower
             if score < self.best_score:
                 self.best_score = score
                 self.best_protein = [(amino.fold, amino.coordinate, amino.previous_amino) for amino in random_folder.protein.get_aminos()]
 
-            # reset protein
+            # Reset protein
             for amino in self.protein.get_aminos()[1:]:
                 amino.reset_amino()
 
-        # set values of protein to best values
+        # Set values of protein to best values
         for i, values in enumerate(self.best_protein):
             self.protein.get_aminos()[i].set_fold(values[0])
             self.protein.get_aminos()[i].set_coordinate(values[1])
@@ -61,22 +61,22 @@ class RandomFolder():
         """
         length = len(self.protein.get_aminos())
 
-        # keep folding until protein is completely folded
+        # Keep folding until protein is completely folded
         while self.folding < length - 1:
             
-            # fold amino and start over if amino folding failed
+            # Fold amino and start over if amino folding failed
             if self.fold_amino() == 1:
                 
                 for amino in self.protein.get_aminos()[1:]:
                     
-                    # reset all values of aminos
+                    # Reset all values of aminos
                     amino.reset_amino()
                 
-                # set fold position back to zero and clear coordinates
+                # Set fold position back to zero and clear coordinates
                 self.folding = 0
                 self.coordinates = set()
 
-        # calculate score and set in protein
+        # Calculate score and set in protein
         score = self.protein.calculate_score()
         self.protein.set_score(score)
 
@@ -86,11 +86,11 @@ class RandomFolder():
         Fold amino by retrieving and setting fold  of current amino,
         and coordinate and previous amino of that amino's next neighbor.
         """
-        # store current amino and its coordinate
+        # Store current amino and its coordinate
         current_amino = self.protein.get_aminos()[self.folding]
         self.coordinates.add(current_amino.coordinate)
 
-        # retrieve values for current amino and its neighbor
+        # Retrieve values for current amino and its neighbor
         new_values = self.get_values(current_amino)
         
         # return 1 if no values are returned
@@ -108,14 +108,14 @@ class RandomFolder():
         """
         Retrieves values for amino by randomly choosing values out of possible values
         """
-        # retrieve all possible values for amino
+        # Retrieve all possible values for amino
         possible_values = self.get_possible_values(amino)
         
-        # return None if no possible values
+        # Return None if no possible values
         if len(possible_values) == 0:
             return None
         
-        # choose random values
+        # Choose random values
         fold, coordinate = random.choice(possible_values)
         
         return fold, coordinate
@@ -127,21 +127,21 @@ class RandomFolder():
         """    
         possible_values = []
 
-        # retrieve values for 2D-amino
+        # Retrieve values for 2D-amino
         if len(amino.coordinate) == 2:
             x, y = amino.coordinate
         
-            # dict with all possible fold - next-coordinate combinations 
+            # Dict with all possible fold - next-coordinate combinations 
             values = {1: (x+1, y), -1: (x-1, y), 2:(x, y+1), -2: (x, y-1)} 
 
-        # retrieve values for 3D amino
+        # Retrieve values for 3D amino
         if len(amino.coordinate) == 3:
             x, y, z = amino.coordinate
 
-            # dict with all possible fold - next-coordinate combinations 
+            # Dict with all possible fold - next-coordinate combinations 
             values = {1: (x+1, y, z), -1: (x-1, y, z), 2:(x, y+1, z), -2: (x, y-1, z), 3: (x, y, z+1), -3: (x, y, z-1)} 
 
-        # add values to list if coordinate is not already occupied
+        # Add values to list if coordinate is not already occupied
         for fold, coordinate in values.items(): 
             if self.is_free_space(coordinate):
                 possible_values.append((fold, coordinate))
@@ -153,7 +153,7 @@ class RandomFolder():
         """
         Checks whether coordinate is already in proteins occupied coordinates
         """
-        # return True if coordinate is not occupied, else False
+        # Return True if coordinate is not occupied, else False
         return coordinate not in self.coordinates
 
 
@@ -162,12 +162,12 @@ class RandomFolder():
         Sets fold of amino with specified id, sets coordinate and 
         previous fold of that amino's next neighbor 
         """
-        # set fold of current amino
+        # Set fold of current amino
         self.protein.get_aminos()[id].set_fold(values[0])
 
-        # set coordinate and previous amino of next amino
+        # Set coordinate and previous amino of next amino
         self.protein.get_aminos()[id + 1].set_coordinate(values[1])
         self.protein.get_aminos()[id + 1].set_previous_amino(-values[0])
         
-        # return score of new configuration
+        # Return score of new configuration
         return self.protein.calculate_score()
